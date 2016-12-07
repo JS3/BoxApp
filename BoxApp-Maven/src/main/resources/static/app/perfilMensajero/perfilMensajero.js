eb7e52343b1d4ad98b4ac2c030c258446746e464
MstrJL300 <joslum3@icloud.com>
2016-12-07 15:25:29 -0500

    Agregar un mensajero funciona

--- BoxApp-Maven/src/main/resources/static/app/perfilMensajero/perfilMensajero.js
+++ BoxApp-Maven/src/main/resources/static/app/perfilMensajero/perfilMensajero.js
@@ -18,47 +18,46 @@ angular.module('myApp.perfilMensajero', ['ngRoute'])
         $scope.agregarSolicitante = function(){                
             if($scope.razon.length > 0 && $scope.id.length > 0){
                 GetSM.get({n:$scope.id},function(data){
-//                console.log("data = ");
-                a = data;
-//                    console.log("data = ");
-//                    var a = data;
-//                    console.log("data = ");
-//                    $scope.solicitante = data;
-//                    var a = data;
-//                    console.info("Solicitante: "+ a.nombre);
-//                    
-//                    var mensajero =
-//                            {   idSolicitante : a.idSolicitante,
-//                                nombre : a.nombre,
-//                                primerApellido : a.primerApellido,
-//                                segundoApellido : a.segundoApellido,
-//                                genero : a.genero,
-//                                cedula : a.cedula,
-//                                edad : a.edad,
-//                                fechaNacimiento : a.fechaNacimiento,
-//                                telefono : a.telefono,
-//                                direccion : a.direccion,
-//                                pais : a.pais,
-//                                municipio  : a.municipio,
-//                                codigoPostal : a.codigoPostal,
-//                                calificacion : '0'
-//                            };
-//                            
-//                    console.log(mensajero);
-//                    mensajeros.save(mensajero, function(){
-//                        console.info("saved   "+ mensajero);
-//                    });  
+                    a = data;
+                    if(a == null){
+                        alert("Ese ID no existe.");
+                        console.log("||||a = "+a);
+                    }
+                    else{
+                        $scope.solicitante = data;
+                        console.info("Solicitante: "+ a.nombre);
+
+                        var mensajero =
+                            {   idSolicitante : a.idSolicitante,
+                                nombre : a.nombre,
+                                primerApellido : a.primerApellido,
+                                segundoApellido : a.segundoApellido,
+                                genero : a.genero,
+                                cedula : a.cedula,
+                                edad : a.edad,
+                                fechaNacimiento : a.fechaNacimiento,
+                                telefono : a.telefono,
+                                direccion : a.direccion,
+                                pais : a.pais,
+                                municipio  : a.municipio,
+                                codigoPostal : a.codigoPostal,
+                                calificacion : '0'
+                            };
+
+                        console.log(mensajero);
+                        mensajeros.save(mensajero, function(){
+                            console.info("saved   "+ mensajero);
+                        }); 
+                        
+                        $scope.mensaje = " ahora es un mensajero.";                       
+                    }  
                 });
-                $scope.mensaje = " ahora es un mensajero.";
                 
     //                    $location.path("/viaje");
-                console.log("a = "+a);
-                if(a == null){
-                    console.log("||||a = "+a);
-                }
+                
             }
             else
-                alert("Por favor, de una razon.");
+                alert("Por favor, de una razon y escriba su ID de BoxApp.");
         }; 
         
 //        $scope.agregarMensajero = function(){     
